fc66cfbd6bf2e27c9207a6acb0d15c880335e739
andresfaj <achuryjaramillo@gmail.com>
2020-05-31 16:04:10 -0500

    app-routing was added and other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { MyProductsComponent } from './components/my-products/my-products.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '', component: AboutMeComponent},
+  {path: '', component: ContactMeComponent},
+  {path: '', component: MyProductsComponent},
+  {path: '', component: BlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { MyProductsComponent } from './components/my-products/my-products.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ContactMeComponent,
+    MyProductsComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
